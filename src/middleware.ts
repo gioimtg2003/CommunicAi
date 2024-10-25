b1189896df284a090421e7eed5bcfad7e8a16c55
Nguyen Cong Gioi <conggioi.pro264@gmail.com>
2024-10-25 20:54:11 +0700

    config format code & eslint

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,6 @@ import createIntlMiddleware from "next-intl/middleware";
 import { NextRequest } from "next/server";
 
 const defaultLocale = "en" as LocaleCode;
-// const PUBLIC_FILE = /\.(.*)$/;
 
 export const intlMiddleware = createIntlMiddleware({
     locales: LocaleList,
@@ -12,13 +11,9 @@ export const intlMiddleware = createIntlMiddleware({
 });
 
 export default function middleware(req: NextRequest) {
-    // const excludePattern = '^(/(' + LocaleList.join('|') + '))?/admin/?.*?$';
-    // const isPublicPage = !PUBLIC_FILE.test(req.nextUrl.pathname);
-
     return intlMiddleware(req);
 }
 
 export const config = {
-    // Skip paths that should not be internationalized
     matcher: ["/((?!api|_next|.*\\..*).*)"],
 };
